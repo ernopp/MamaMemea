0d93eee92aef4a4e710ab09a0b1f11e4feff78cc
Ernest Oppetit <2433131+ernopp@users.noreply.github.com>
2023-04-04 22:56:33 +0000

    MODE can be live or daily

--- main.py
+++ main.py
@@ -21,53 +21,58 @@ headers = {"Authorization": f"Client-ID {client_id}"}
 
 
 def get_top_image_url():
-    # Get the top images from Imgur in the last day
-    response = requests.get(
-        "https://api.imgur.com/3/gallery/t/funny/time/1",
-        headers=headers,
-    )
-    items = response.json()["data"]["items"]
+  # Get the top images from Imgur in the last day
+  response = requests.get(
+    "https://api.imgur.com/3/gallery/t/funny/time/1",
+    headers=headers,
+  )
+  items = response.json()["data"]["items"]
 
-    # Find the first image post
-    for item in items:
-        if item["is_album"]:
-            continue
-        return item["link"]
-    return None
+  # Find the first image post
+  for item in items:
+    if item["is_album"]:
+      continue
+    return item["link"]
+  return None
 
 
 def send_image(image_url):
-    if image_url:
-        # Send the image to the Whatsapp bot
-        message = client.messages.create(
-            media_url=image_url,
-            from_=from_number,
-            to=to_number,
-        )
-        print("Image sent!")
-    else:
-        print("Failed to fetch image.")
+  if image_url:
+    # Send the image to the Whatsapp bot
+    message = client.messages.create(
+      media_url=image_url,
+      from_=from_number,
+      to=to_number,
+    )
+    print("Image sent!")
+  else:
+    print("Failed to fetch image.")
 
 
 def job():
-    top_image_url = get_top_image_url()
-    send_image(top_image_url)
-    print("Test")
+  top_image_url = get_top_image_url()
+  send_image(top_image_url)
+  print("Test")
 
 
 def run_at_time(h, m, tz):
-    local_timezone = timezone(tz)
-    now = datetime.now(local_timezone)
-    target_time = now.replace(hour=h, minute=m, second=0, microsecond=0)
-    if now > target_time:
-        target_time += timedelta(days=1)
-    delay = (target_time - now).total_seconds()
-    time.sleep(delay)
-    job()
+  local_timezone = timezone(tz)
+  now = datetime.now(local_timezone)
+  target_time = now.replace(hour=h, minute=m, second=0, microsecond=0)
+  if now > target_time:
+    target_time += timedelta(days=1)
+  delay = (target_time - now).total_seconds()
+  time.sleep(delay)
+  job()
 
 
 if __name__ == "__main__":
+  mode = os.environ.get("MODE", "live")
+
+  if mode == "live":
+    job()
+  else:
     schedule.every().day.at("10:30:00").do(run_at_time, 10, 30, "CET")
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+      schedule.run_pending()
+      time.sleep(1)
